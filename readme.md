3697b93f74527ddce99da12e2c80e9e83567b3ae
Malloc72P <scra1028@gmail.com>
2023-04-18 00:18:36 +0900

    힝 잘 안되넹

---
+++ readme.md
@@ -0,0 +1,8 @@
+https://www.npmjs.com/package/babel-plugin-transform-umd-to-iife
+
+Browserify로 노드기반 모듈을 브라우저에서 동작가능하도록 변환
+
+변환된 모듈은 UMD, 원래라면 여기서 만족해야하는데, UMD가 싫다.
+UMD를 IIFE로 바꾸려면...?
+
+바벨과 위의 링크에 있는 플러그인을 사용해서 UMD 모듈을 IIFE로 변환해서 내보낸다면...?
--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,40 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import polyfill from 'rollup-plugin-polyfill';
 
 
-export default {
-    input: 'src/main.js',
+// export default {
+//     input: 'src/main.js',
+//     output: {
+//       format: 'iife',
+//       file: 'testing-bed/public/rollup-bundle.js',
+//       globals: {
+//       },
+//     },
+//     plugins: [
+//         commonjs(), 
+//         // inject({ process: 'process', global: 'window' }), 
+//         nodeResolve({
+//             preferBuiltins: false,
+//             mainFields: ['module', 'jsnext:main', 'browser'],
+//           }),
+//         json(),
+//         polyfill([])
+//         // nodePolyfills({include: ['process']})
+//     ],
+//   };
+
+
+export default [
+  {
+    input : 'staging/pdfkit.js',
     output: {
       format: 'iife',
-      file: 'testing-bed/public/rollup-bundle.js',
-      globals: {
-      },
-    },
-    plugins: [
-        commonjs(), 
-        // inject({ process: 'process', global: 'window' }), 
-        nodeResolve({
-            preferBuiltins: false,
-            mainFields: ['module', 'jsnext:main', 'browser'],
-          }),
-        json(),
-        polyfill([])
-        // nodePolyfills({include: ['process']})
-    ],
-  };
+      file: 'testing-bed/public/bundled-pdfkit.js'
+    }
+  },
+  {
+    input : 'staging/blob-stream.js',
+    output: {
+      format: 'iife',
+      file: 'testing-bed/public/bundled-blob-stream.js'
+    }
+  }
+]
